aadd2c6c5dcd3c34a46102b68cd517631c467f52
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-03-31 20:50:30 -0700

    (not) update styling

--- src/OldDraggableCalendar.js
+++ src/OldDraggableCalendar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './OldDraggableCalendar.css'
 
@@ -8,16 +9,18 @@ const todoList = [{time: "day 1", content: "eat"}, {time: "day 2", content: "sle
 const OldDraggableCalendar = () => (
     <Container id="calendar">
         <h1>Calendar</h1>
-        <ul>{
-            todoList.map((todo) => (
-                <div className={"box"}>
-                    <div className={"item"} id={"drag" + Math.random() * 1000} draggable={"true"}>
-                        <input className={"btn btn-primary"} type={"button"} data-bs-toggle={"popover"} data-bs-trigger={"focus"} title={todo.time}
-                               data-bs-content={todo.content} value={todo.content} data-bs-original-title={"1:00AM"} aria-label={"1:00AM"}/>
+        <Row>
+            {todoList.map((todo) => (
+                <Col>
+                    <div className={"box"}>
+                        <div className={"item"} id={"drag" + Math.random() * 1000} draggable={"true"}>
+                            <input className={"btn btn-primary"} type={"button"} data-bs-toggle={"popover"} data-bs-trigger={"focus"} title={todo.time}
+                                   data-bs-content={todo.content} value={todo.content} data-bs-original-title={"1:00AM"} aria-label={"1:00AM"}/>
+                        </div>
                     </div>
-                </div>
-            ))
-        }</ul>
+                </Col>
+            ))}
+        </Row>
     </Container>
 )
 
