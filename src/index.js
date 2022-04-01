f7eb92a473c7d709b19da7a3b2965f69926562e0
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-03-31 20:25:39 -0700

    give up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import DraggableCalendar from "./DraggableCalendar";
+import OldDraggableCalendar from "./OldDraggableCalendar";
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<DraggableCalendar />, document.getElementById('root'));
+ReactDOM.render(<OldDraggableCalendar />, document.getElementById('root'));
